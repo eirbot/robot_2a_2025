53f22c68ebcc1eecef477e5faa47e7444d6de7ce
chevalfromage <ant.thiery@free.fr>
2025-02-13 17:59:17 +0100

    les 3 threads fonctionnent

--- src/main.cpp
+++ src/main.cpp
@@ -6,6 +6,9 @@
 
 #include "BluetoothSerial.h"
 
+#define dRoues 100.0
+#define stepPerRev 3200
+#define ecartRoues 253.0
 
 #if !defined(CONFIG_BT_ENABLED) || !defined(CONFIG_BLUEDROID_ENABLED)
 #error Bluetooth is not enabled! Please run `make menuconfig` to and enable it
@@ -47,10 +50,38 @@ void moteur_gauche(int vitesse,int sens){
   digitalWrite(IN3, sens);
 }
 
+void vavancer(void *pvParameters){
+  digitalWrite(IN1,HIGH);
+  digitalWrite(IN3,HIGH);
+  int steps = (*(int *)pvParameters / (3.14 * dRoues)) * stepPerRev;
+  for(int k=0 ; k < steps; k++){
+    digitalWrite(ENA,HIGH);
+    digitalWrite(ENB,HIGH);
+    delayMicroseconds(100);
+    digitalWrite(ENA,LOW);
+    digitalWrite(ENB,LOW);
+    delayMicroseconds(1000);
+  }
+  vTaskDelete(NULL);
+}
+
+void vtourner(void *pvParameters){
+  digitalWrite(IN1,HIGH);
+  digitalWrite(IN3,LOW);
+  int steps = (*(int*)pvParameters / 360.0) * (3.14 * ecartRoues) * stepPerRev / (3.14 * dRoues);
+  for(int k=0 ; k < steps; k++){
+    digitalWrite(ENA,HIGH);
+    digitalWrite(ENB,HIGH);
+    delay(1);
+    digitalWrite(ENA,LOW);
+    digitalWrite(ENB,LOW);
+    delay(1);
+  }
+  vTaskDelete(NULL);
+}
 
 void vcontrole_bluetooth(void *pvParameters)
 {
-
     for( ;; )
     {
       while(SerialBT.available()){
@@ -91,30 +122,54 @@ void setup() {
   SerialBT.begin("ESP32test");
   SerialBT.setTimeout(50);
 
-
-  ledcSetup(ENA, 1000,resolution);
-  ledcAttachPin(ENA,1);
+  // ledcSetup(ENA, 1000,resolution);
+  // ledcAttachPin(ENA,1);
+  pinMode(ENA,OUTPUT);
   pinMode(IN1,OUTPUT);
 
-  ledcSetup(ENB, 1000,resolution);
-  ledcAttachPin(ENB,2);
+  // ledcSetup(ENB, 1000,resolution);
+  // ledcAttachPin(ENB,2);
+  pinMode(ENB,OUTPUT);
   pinMode(IN3,OUTPUT);
 
-  xTaskCreate(
-    vcontrole_bluetooth, /* Task function. */
-    "vcontrole_bluetooth", /* name of task. */
-    1000, /* Stack size of task */
-    NULL, /* parameter of the task */
-    1, /* priority of the task */
-    NULL); /* Task handle to keep track of created task */
-
+  // xTaskCreate(
+  //   vcontrole_bluetooth, /* Task function. */
+  //   "vcontrole_bluetooth", /* name of task. */
+  //   1000, /* Stack size of task */
+  //   NULL, /* parameter of the task */
+  //   1, /* priority of the task */
+  //   NULL/* Task handle to keep track of created task */
+  // ); 
+  
+  
+  delay(2000);
+  
 }
 
 
-
+//un tour = 3200 step
 
 void loop() {
+  int distance = 1000;
+  int angle=90;
+  // xTaskCreate(
+  //   vavancer, /* Task function. */
+  //   "vavancer", /* name of task. */
+  //   1000, /* Stack size of task */
+  //   &distance, /* parameter of the task */
+  //   1, /* priority of the task */
+  //   NULL/* Task handle to keep track of created task */
+  // ); 
 
 
+  xTaskCreate(
+    vtourner, /* Task function. */
+    "vtourner", /* name of task. */
+    1000, /* Stack size of task */
+    &angle, /* parameter of the task */
+    1, /* priority of the task */
+    NULL/* Task handle to keep track of created task */
+  ); 
+  delay(1000000);
   
 }
