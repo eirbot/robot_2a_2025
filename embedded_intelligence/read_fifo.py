1703dddc9ec3a211700a0cb952635edfa7fcc942
Convolutio <79534557+Convolutio@users.noreply.github.com>
2025-05-20 15:44:43 +0200

    feat: full poetry 2.1 project with scripts

--- embedded_intelligence/read_fifo.py
+++
@@ -1,8 +0,0 @@
-with open("/tmp/fifo_out", "r") as f:
-    while True:
-        try:
-            line = f.readline()
-            if len(line) > 0:
-                print(line, end="")
-        except* Exception:
-            pass
