b8121947059da015121ad76ef3117f05333c8b18
Convolutio <79534557+Convolutio@users.noreply.github.com>
2025-05-20 13:03:40 +0200

    feat: abstract communication structure + fifo
    a complete asyncio io protocol is implemented
    with a read write into two fifos

--- embedded_intelligence/main.py
+++ embedded_intelligence/main.py
@@ -1,3 +1,4 @@
+import asyncio
 from world import World
 from firmware import Robot
 
@@ -7,6 +8,17 @@ def loop():
 def setup():
     pass
 
+def strategy(robot: Robot):
+    async def robot_loop_func():
+        await robot.goTo(2, 1, 10)
+        await robot.goTo(3, 3, 10)
+        await robot.actionCanette()
+    return robot_loop_func
+
 def start():
-    robot = Robot()
+    robot = Robot((1, 2))
     game = World()
+    asyncio.run(robot.connect_and_run(strategy(robot)))
+
+if __name__ == "__main__":
+    start()
