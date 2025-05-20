a7f2f51e3db03ea9eef17d0755dd0098dbe8614e
Convolutio <79534557+Convolutio@users.noreply.github.com>
2025-05-20 18:55:13 +0200

    not worked: channel opening and closing
    for fifo, there is blocking thread issue (again)

--- embedded_intelligence/src/cfr_2025_2a_embedded_ai/communication/com.py
+++ embedded_intelligence/src/cfr_2025_2a_embedded_ai/communication/com.py
@@ -3,10 +3,19 @@ import asyncio
 from queue import SimpleQueue
 from typing import Mapping, Optional, Tuple, cast
 
+class TerminateReadLoop(Exception):
+    """Exception raised to ends the reading loop when the robot has
+    finished"""
+
 ReplyPrefix = str
 
 AwaitingString = list[Optional[str]]
 
+"""
+Usage instruction for a safe channel opening/closing:
+Only write messages when the read_loop has started running. There is a
+protection that will block any message sending while the channel is not open
+"""
 class Communication(ABC):
     """
     - read_yield_frequency: this is a duration in milliseconds. After each
@@ -22,6 +31,7 @@ class Communication(ABC):
             for anticipatedAnswerPrefix in anticipatedAnswerPrefixes
         }
         self._reading_thread_sleep_time_after_reading_try = read_yield_frequency
+        self._is_channel_open = asyncio.Event()
 
     @classmethod
     def __create_empty_awaiting_string(cls) -> AwaitingString:
@@ -48,31 +58,54 @@ class Communication(ABC):
     async def _blocking_write(self, message: str) -> None:
         pass
 
+    @abstractmethod
+    async def _open_channel(self) -> None:
+        pass
+
+    @abstractmethod
+    async def _close_channel(self) -> None:
+        pass
+
     def _split(self, msg: str) -> Tuple[ReplyPrefix, str]:
         splitted = msg.split(" ")
         return splitted[0], " ".join(splitted[1:])
 
-    async def read_loop(self):
+    async def read_loop(self) -> None:
         msg: str = ""
-        while True:
-            new_msg: list[str] = (await self._frequently_yielding_read()).split("\n")
-            while len(new_msg) > 1:
-                msg += new_msg.pop(0)
-                prefix, return_message = self._split(msg)
-                if prefix in self._queue:
-                    q = self._queue[prefix]
-                    if not q.empty():
-                        waiting_event, waiting_answer = self._queue[prefix].get_nowait()
-                        # Returning the given string
-                        Communication.__set_awaiting_string(waiting_answer,
-                                                   return_message)
-                        # And setting the waiting event has done
-                        # The next yielding read will yield to the process
-                        waiting_event.set()
-                msg = ""
-            msg += new_msg[0]
+        await self._open_channel()
+        print("Communication channel open! Starting reading loop...")
+        self._is_channel_open.set()
+        try:
+            while True:
+                new_msg: list[str] = (await self._frequently_yielding_read()).split(
+                    "\n"
+                )
+                while len(new_msg) > 1:
+                    msg += new_msg.pop(0)
+                    prefix, return_message = self._split(msg)
+                    if prefix in self._queue:
+                        q = self._queue[prefix]
+                        if not q.empty():
+                            waiting_event, waiting_answer = self._queue[
+                                prefix
+                            ].get_nowait()
+                            # Returning the given string
+                            Communication.__set_awaiting_string(
+                                waiting_answer, return_message
+                            )
+                            # And setting the waiting event has done
+                            # The next yielding read will yield to the process
+                            waiting_event.set()
+                    msg = ""
+                msg += new_msg[0]
+        except TerminateReadLoop:
+            # the read loop is over, we do not use anymore the instance
+            self._is_channel_open.clear()
+            await self._close_channel()
+            print("End of reading loop. Communication channel closed!")
 
     async def send(self, msg: str, expected_reply_prefix: ReplyPrefix) -> str:
+        await self._is_channel_open.wait() # do not write while channel is closed 
         wait_reception_event = asyncio.Event()
         expectedAnswer = Communication.__create_empty_awaiting_string()
         self._queue[expected_reply_prefix].put((wait_reception_event,
