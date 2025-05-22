300ede1fadf17c65832ea5cbf77f9fab3043740c
Convolutio <79534557+Convolutio@users.noreply.github.com>
2025-05-22 08:52:07 +0200

    feat: pretty logging with debug and info levels
    TODO: setup colors in function of the R/S loop, not the debug level
    TODO: restore the debug level
    TODO: setup a debug environment variable
    
    TODO: make the exception handling for closing read loop run

--- embedded_intelligence/src/cfr_2025_2a_embedded_ai/firmware.py
+++ embedded_intelligence/src/cfr_2025_2a_embedded_ai/firmware.py
@@ -9,6 +9,8 @@ from .communication.com import Communication, TerminateReadLoop
 from .communication.fifo_com import FifoCom
 from .communication.serial_com import SerialCom
 
+from .debug_log import print_debug_log, print_log
+
 Position = Tuple[int, int]
 
 READ_YIELDING_TIME = 2
@@ -36,13 +38,14 @@ class Robot:
         raise TerminateReadLoop()
 
     async def connect_and_run(self, robot_loop_func: Callable[[], Coroutine]):
+        print_log("Starting in concurrence robot's strategy loop (S) and communication reading loop (R)")
         try:
             # run robot loop and reading in concurrency
             async with asyncio.TaskGroup() as tg:
                 tg.create_task(self._run_loop_and_terminate_group(robot_loop_func))
                 tg.create_task(self._com.read_loop())
         except* TerminateReadLoop:
-            print("Robot's strategy loop and communication reading loop finished!")
+            print_log("Robot's strategy loop and communication reading loop finished!")
 
     @property
     def p(self):
@@ -50,17 +53,17 @@ class Robot:
 
     async def goTo(self, x: int, y: int, angle: float) -> None:
         try:
-            print("S" + "-"*5 + f"Sending GoTo with these paramtres: {x} {y} {angle}" + "-"*5)
+            print_debug_log("S" + "-"*5 + f"Sending GoTo with these paramtres: {x} {y} {angle}" + "-"*5)
             await self._com.send(f"G {x} {y} {angle}", Robot.GOTO_ANSWER_PREFIX)
             self._p = (x, y)
-            print("S" + "-"*5 + f"GoTo done! Robot in position {self._p}" + "-"*5)
+            print_debug_log("S" + "-"*5 + f"GoTo done! Robot in position {self._p}" + "-"*5)
         except asyncio.TimeoutError:
-            print("S" + "-"*5 + f"Timeout in sending GoTo {x} {y} {angle}" + "-"*5)
+            print_debug_log("S" + "-"*5 + f"Timeout in sending GoTo {x} {y} {angle}" + "-"*5)
 
     async def actionCanette(self) -> None:
         try:
-            print("S" + "-"*5 + "Sending CanetteAction" + "-"*5)
+            print_debug_log("S" + "-"*5 + "Sending CanetteAction" + "-"*5)
             await self._com.send("C", Robot.CANETTE_ACTION_ANSWER_PREFIX)
-            print("S" + "-"*5 + "CanetteAction confirmed by esp" + "-"*5)
+            print_debug_log("S" + "-"*5 + "CanetteAction confirmed by esp" + "-"*5)
         except asyncio.TimeoutError:
-            print("S" + "-"*5 + "Timeout in sending CanetteAction" + "-"*5)
+            print_debug_log("S" + "-"*5 + "Timeout in sending CanetteAction" + "-"*5)
